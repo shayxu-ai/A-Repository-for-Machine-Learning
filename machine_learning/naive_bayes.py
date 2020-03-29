9d00380d1fdbb5cbee768b43e410bc4eeb894013
Yuan XU <Yxu9428@163.com>
2020-03-29 23:44:50 +0800

    naive bayes

---
+++ machine_learning/naive_bayes.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer  # 从sklearn.feature_extraction.text里导入文本特征向量化模块
+from sklearn.naive_bayes import MultinomialNB  # 从sklean.naive_bayes里导入朴素贝叶斯模型
+from sklearn.metrics import classification_report
+
+X = np.array([
+    [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3],
+    [4, 5, 5, 4, 4, 4, 5, 5, 6, 6, 6, 5, 5, 6, 6]
+])
+X = X.T
+# y = np.array([-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1])
+y = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1])
+
+# In[8]:
+
+
+# 2.数据预处理：训练集和测试集分割，文本特征向量化
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=33)  # 随机采样25%的数据样本作为测试集
+print(X_train)  # 查看训练样本
+print(y_train)  # 查看标签
+
+# 文本特征向量化
+# vec = CountVectorizer()
+# X_train = vec.fit_transform(X_train)
+# X_test = vec.transform(X_test)
+
+# 3.使用朴素贝叶斯进行训练
+mnb = MultinomialNB()  # 使用默认配置初始化朴素贝叶斯
+mnb.fit(X_train, y_train)  # 利用训练数据对模型参数进行估计
+y_predict = mnb.predict(X_test)  # 对参数进行预测
+
+# 4.获取结果报告
+print('The Accuracy of Naive Bayes Classifier is:', mnb.score(X_test, y_test))
+print(classification_report(y_test, y_predict))
+
+# In[ ]:
--- nlp_tutorial/NNLM-Tensor.py
+++ nlp_tutorial/NNLM-Tensor.py
@@ -8,41 +8,43 @@
 
 """
     https://github.com/graykode/nlp-tutorial
-
+    Neural Network Language Model
     酷诶
+    很奇怪的问题是，我公司里的tf是v1.0吗？我记得我看过是v2.0的啊
 
 """
 
 import tensorflow as tf
 import numpy as np
 
-tf.reset_default_graph()
+# 清除默认图形堆栈并重置全局默认图形
+# tf.compat.v1.reset_default_graph()
 
 sentences = ["i like dog", "i love coffee", "i hate milk"]
 
 word_list = " ".join(sentences).split()
 word_list = list(set(word_list))
+# 以词为键值
 word_dict = {w: i for i, w in enumerate(word_list)}
+# 以序号为键值
 number_dict = {i: w for i, w in enumerate(word_list)}
-n_class = len(word_dict) # number of Vocabulary
+
+n_class = len(word_dict)    # number of Vocabulary
 
 # NNLM Parameter
-n_step = 2 # number of steps ['i like', 'i love', 'i hate']
-n_hidden = 2 # number of hidden units
+n_step = 2  # number of steps ['i like', 'i love', 'i hate']
+n_hidden = 2    # number of hidden units
 
-def make_batch(sentences):
-    input_batch = []
-    target_batch = []
+# 准备训练数据
+input_batch = list()
+target_batch = list()
+for sen in sentences:
+    word = sen.split()  #   [i, like, dog]
+    input_ = [word_dict[n] for n in word[:-1]]
+    target = word_dict[word[-1]]
 
-    for sen in sentences:
-        word = sen.split()
-        input = [word_dict[n] for n in word[:-1]]
-        target = word_dict[word[-1]]
-
-        input_batch.append(np.eye(n_class)[input])
-        target_batch.append(np.eye(n_class)[target])
-
-    return input_batch, target_batch
+    input_batch.append(np.eye(n_class)[input_])
+    target_batch.append(np.eye(n_class)[target])
 
 # Model
 X = tf.placeholder(tf.float32, [None, n_step, n_class]) # [batch_size, number of steps, number of Vocabulary]
@@ -54,19 +56,18 @@ d = tf.Variable(tf.random_normal([n_hidden]))
 U = tf.Variable(tf.random_normal([n_hidden, n_class]))
 b = tf.Variable(tf.random_normal([n_class]))
 
-tanh = tf.nn.tanh(d + tf.matmul(input, H)) # [batch_size, n_hidden]
-model = tf.matmul(tanh, U) + b # [batch_size, n_class]
+tanh = tf.nn.tanh(d + tf.matmul(input, H))  # [batch_size, n_hidden]
+model = tf.matmul(tanh, U) + b  # [batch_size, n_class]
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
-prediction =tf.argmax(model, 1)
+prediction = tf.argmax(model, 1)
 
 # Training
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-input_batch, target_batch = make_batch(sentences)
 
 for epoch in range(5000):
     _, loss = sess.run([optimizer, cost], feed_dict={X: input_batch, Y: target_batch})
@@ -74,7 +75,7 @@ for epoch in range(5000):
         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
 
 # Predict
-predict =  sess.run([prediction], feed_dict={X: input_batch})
+predict = sess.run([prediction], feed_dict={X: input_batch})
 
 # Test
 input = [sen.split()[:2] for sen in sentences]
