3556fe296ceff30bfdfe9823d79c776eec5dd3c7
Yuan XU <Yxu9428@163.com>
2020-04-15 00:41:35 +0800

    hmm

---
+++ 测试/operator_hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/14 10:46 下午
+# @Author : 徐缘
+# @FileName: operator_hello.py
+# @Software: PyCharm
+
+
+a = 0b100
+
+b = 0b011
+
+print(a, b)
+print(a & b)    # 与
+print(a | b)    # 或
+print(a ^ b)    # 异或
+print(~a)       # 非
