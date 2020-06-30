58330b56b2f04419a2c27cefc254be18c05ee66e
CMCCxuSir <yxu9428@163.com>
2020-06-30 17:07:21 +0800

    ai contest

--- contest/base_station_out_service_prediction/bakup/chaoxi.py
+++ contest/base_station_out_service_prediction/bakup/chaoxi.py
@@ -26,21 +26,22 @@ from tqdm import tqdm
 # import datetime
 
 """
-    https://www.cnblogs.com/peng104/p/10398490.html
+
 """
 
-test_0322_path = '/mnt/5/Alert_BTS_HW_0316-0322'
-test_0330_path = '/mnt/5/Alert_BTS_HW_0324-0330'
+test_0322_path = '/mnt/5/Alert_BTS_HW_0316-0322'    # 测试集1
+test_0330_path = '/mnt/5/Alert_BTS_HW_0324-0330'    # 测试集2
 
 all_test_data = pd.DataFrame()
 all_test_data2 = pd.DataFrame()
 
+# 将测试集1目录下的
 for now_csv in tqdm(os.listdir(test_0322_path)):
     data = pd.read_csv(os.path.join(test_0322_path, now_csv))
     all_test_data = all_test_data.append(data)
     tmp1 = all_test_data[(all_test_data['告警名称'] == '小区不可用告警') |
                          (all_test_data['告警名称'] == '网元连接中断')]
-    tmp1_label1_IDs = tmp1['基站名称'].unique()
+    tmp1_label1_IDs = tmp1['基站名称'].unique()     # 出过退服告警的基站
 
 for now_csv in tqdm(os.listdir(test_0330_path)):
     data = pd.read_csv(os.path.join(test_0330_path, now_csv))
